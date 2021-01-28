b62c91277c711702fc5957496f4d34055719ffb6
Andrii Danilishin <a.i.danilishin@gmail.com>
2021-01-28 16:49:07 +0200

    first commit

---
+++ src/main/java/com/dan/depotato/DepotatoApplication.java
@@ -0,0 +1,13 @@
+package com.dan.depotato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepotatoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DepotatoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/dan/depotato/DepotatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dan.depotato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepotatoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
